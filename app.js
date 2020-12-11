37a433e72e694ebb6d82e77c7383f3e1fd3aa134
darrell <dcbacarro@codedisruptors.com>
2020-12-11 20:02:58 +0800

    Finalize app

--- app.js
+++ app.js
@@ -7,28 +7,44 @@ const { execFile } = require('child_process');
 
 const app = express();
 
-const opener = () => {
-  const encoder = new EscPosEncoder();
-  const code = encoder
-    .initialize()
-    .codepage('windows1251')
-    .raw([0x1B, 0x70, 0x30, 0x37, 0x79])
-    .encode();
+const executor = (path) => new Promise((resolve, reject) => {
+  execFile('print', [path], { windowsHide: true }, (err, stdout, stderr) => {
+    console.log(stdout);
 
-  const path = `opener.bin`;
-  fs.writeFile(path, code, () => {
-    execFile('print', [path], { windowsHide: true });
+    if (stdout.indexOf('PRN') !== -1) reject();
+    else resolve();
+  });
+});
+
+const retry = (path) => new Promise((resolve) => {
+  executor(path)
+    .then(() => {
+      resolve();
+    })
+    .catch((e) => {
+      retry(path);
+    })
+});
+
+const opener = () => {
+  const text = String.fromCharCode(0x1B, 0x70, 0x30, 0x37, 0x79);
+
+  const path = `opener.txt`;
+  fs.writeFile(path, text, () => {
+    retry(path);
   });
 }
 
-const printer = (data) => {
+const printer = (data, reprint = false) => {
   const line = Array(40).fill('-').join('');
   const { transactionData: t, transactionItems: i } = data;
-  opener();
 
-  let text = '         Los Manggalenos Store\n';
+  let text = '';
+  if (!reprint) text += String.fromCharCode(0x1B, 0x70, 0x30, 0x37, 0x79);
+
+  text += '\n         Los Manggalenos Store\n';
   text += '          Brgy. Manggalang I,\n';
-  text += '             Sariaya, Quezon\n';
+  text += '            Sariaya, Quezon\n';
   text += line + '\n';
   text += `Receipt No.: ${t.transactionNumber}\n`;
   text += `Cashier: ${t.cashier}\n`;
@@ -55,16 +71,16 @@ const printer = (data) => {
   text += '\nChange Due:         ';
   text += parseFloat(`${t.tenderChange}`).toFixed(2).padStart(20, ' ');
 
-  text += '\n\n     THIS IS NOT AN OFFICIAL RECEIPT\n\n';
-  text += '     Thank you for shopping with us!\n\n';
+  text += '\n\n     THIS IS NOT AN OFFICIAL RECEIPT\n';
+  text += '     Thank you for shopping with us!\n';
   text += '        ' + moment().format('MMM DD, YYYY hh:mm:ss A');
-  text += '\n\n\n\n\n\n'
+  text += '\n\n\n\n\n\n\n\n\n'
 
   const filename = `${t.transactionNumber}`.split('-').join('').padStart(12, '0');
 
   const path = `${filename}.txt`;
   fs.writeFile(path, text, () => {
-    execFile('print', [path], { windowsHide: true });
+    retry(path);
   });
 }
 
@@ -72,6 +88,11 @@ app.use(bodyParser.json());
 
 app.post('/', async (req, res) => {
   printer(req.body);
+  res.json({ success: true });
+});
+
+app.post('/reprint', async (req, res) => {
+  printer(req.body, true);
   res.json({ success: true });
 });
 
